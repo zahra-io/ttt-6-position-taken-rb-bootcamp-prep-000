9354887ebe18ea83c5a4fa70ff999ede101884b7
Zahra Khan <zarssskhan@gmail.com>
2017-10-05 06:57:26 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,5 +3,6 @@ def position_taken?(board, index)
   if board[index] == " " || board[index] == "" || board[index] == nil
     false
   elsif board[index] == "X" || board[index] == "O"
+    true 
   end
 end
